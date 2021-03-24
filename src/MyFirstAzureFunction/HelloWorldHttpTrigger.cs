dc93fdf6745e5322865be2d46a23518cf488e6e9
Marc Duiker <marcduiker@gmail.com>
2021-03-24 10:27:25 +0100

    Add app settings example

--- src/MyFirstAzureFunction/HelloWorldHttpTrigger.cs
+++ src/MyFirstAzureFunction/HelloWorldHttpTrigger.cs
@@ -1,10 +1,11 @@
+using System;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using System.Net.Http;
 
 namespace AzureFunctionsUniversity.Demo
 {
@@ -36,12 +37,13 @@ namespace AzureFunctionsUniversity.Demo
             ObjectResult result;
             if(string.IsNullOrEmpty(name))
             {
-                var responseMessage = "Pass a name in the query string or in the request body for a personalized response.";
-                result = new BadRequestObjectResult(responseMessage);
+                var failMessage = Environment.GetEnvironmentVariable("Message.Fail");
+                result = new BadRequestObjectResult(failMessage);
             }
             else
             {
-                var responseMessage = $"Hello, {name}. This HTTP triggered function executed successfully.";
+                var successMessage = Environment.GetEnvironmentVariable("Message.Success");
+                var responseMessage = $"Hello, {name}. {successMessage}";
                 result = new OkObjectResult(responseMessage);
             }
 
